038bbb02f8c9618a8e55fe5aa36f9c5325b45dea
surajsharma7 <34912824+surajsharma7@users.noreply.github.com>
2017-12-28 16:43:24 +0530

    Sort

---
+++ Sort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Sort
+{
+	public static void main(String[] args) {
+		int[] num={6,3,8,2,54,1};
+		int i,j,temp;
+		
+		System.out.println("Before");
+		for(i=1;i<=num.length;i++)
+		{
+			System.out.println(+num[i]+" ");
+		}
+		System.out.println("After Sort");
+		for(i=0;i<=num.length-1;i++)
+		{
+			for(j=i;j<num.length-i-1;i++)
+			{
+				if(num[j]>num[j+1])
+				{
+					temp=num[j];
+					num[j]=num[j+1];
+					num[j+1]=num[j];
+					System.out.println(i);
+
+				}
+			}
+		}
+		
+	}
+}
